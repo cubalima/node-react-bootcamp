1e3e3cace9b8a1822c86a9921482959f8c91923b
Angel Lima Martin <roquenublo1997@gmail.com>
2026-07-26 00:47:02 +0100

    feat: configure Express server

---
+++ express-basics/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import employeeRoutes from "./routes/employees";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use("/employees", employeeRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
