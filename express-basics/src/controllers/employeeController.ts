62f66dbffb1e06d1edbfc602c799a61349cd0df5
Angel Lima Martin <roquenublo1997@gmail.com>
2026-07-26 13:59:07 +0100

    refactor: reuse employee service instance

--- express-basics/src/controllers/employeeController.ts
+++ express-basics/src/controllers/employeeController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import EmployeeService from "../services/employeeService.js";
 import { Employee } from "../models/employee.js";
+
+const employeeService = new EmployeeService();
+
 export const getAllEmployees = (req: Request, res: Response): void => {
-    const employeeService = new EmployeeService();
     const employees: Employee[] = employeeService.getAll();
     res.json(employees);
 };
