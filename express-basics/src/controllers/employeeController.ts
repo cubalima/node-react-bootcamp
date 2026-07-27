245406edbb879876e06330141058c7daff450d2c
Angel Lima Martin <roquenublo1997@gmail.com>
2026-07-27 23:40:58 +0100

    ignore

--- express-basics/src/controllers/employeeController.ts
+++ express-basics/src/controllers/employeeController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import EmployeeService from "../services/employeeService.js";
 import { CreateEmployee, Employee } from "../models/employee.js";
-import { ParsedQs } from "qs";
 
 const employeeService = new EmployeeService();
 interface EmployeeParams {
